ef480a469e7f39bd0785d601f7f887bbac30c3eb
Bumkyu <bombkyu@gmail.com>
2018-04-23 09:38:32 +0900

    Coding the UI Part Two - Input and ScrollView

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform, ScrollView } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import ToDo from './ToDo';
 
 const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
+
+  state = {
+    newToDo:""
+  };
   render() {
+    const {newToDo} = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"new To Do"}/>
+          <TextInput 
+            style={styles.input} 
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._controlNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
+
         </View>
       </View>
       
     );
+  }
+
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+    })
   }
 }
 
@@ -53,5 +77,11 @@ const styles = StyleSheet.create({
       }
     }
     )
+  },
+  input: {
+    padding:20,
+    borderBottomColor:"#bbb",
+    borderBottomWidth:1,
+    fontSize:25
   }
 });
---
+++ ToDo.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {View,Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+
+export default class ToDo extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Hello, I'm a new ToDo</Text>
+            </View>
+        );
+    }
+}
+
+const style = StyleSheet.create({})
